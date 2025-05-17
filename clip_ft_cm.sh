59a8c4a8ad7b3d5303cb5a21de8e1bad7eef66bf
tarabfish <piscibussitula@gmail.com>
2025-05-16 23:56:42 -0400

    altered batch size=16

--- clip_ft_cm.sh
+++ clip_ft_cm.sh
@@ -14,7 +14,7 @@ fusion_bench \
     method.num_steps=4000 \
     method.save_interval=2000 \
     method.learning_rate=1e-5 \
-    method.batch_size=4 \
+    method.batch_size=16 \
     modelpool=clip-finetune_${TASK_12}\
     fabric.devices=1 \
     fabric.loggers.root_dir=${SCRIPT_DIR}/outputs/${MODEL_SHORT_NAME} \
@@ -30,7 +30,7 @@ fusion_bench \
         method.num_steps=4000 \
         method.save_interval=2000 \
         method.learning_rate=1e-5 \
-        method.batch_size=4 \
+        method.batch_size=16 \
         modelpool=clip-finetune_${TASK+21} \
         fabric.devices=1 \
         fabric.loggers.root_dir=${SCRIPT_DIR}/output/${MODEL_SHORT_NAME}/${TASK_21} \
