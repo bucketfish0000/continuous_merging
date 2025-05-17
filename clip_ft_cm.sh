502953a2f4a1b61f2ab5342b64597d0192f4cc83
tarabfish <piscibussitula@gmail.com>
2025-05-17 00:27:32 -0400

    changed spelling error

--- clip_ft_cm.sh
+++ clip_ft_cm.sh
@@ -47,28 +47,28 @@ fusion_bench \
     method=dummy \
     modelpool=CLIPVisionModelPool/clip-vit-base-patch32_individual \
     +modelpool.modelpool.models._pretrained_.pretrained_model_name_or_path="${SCRIPT_DIR}/outputs/${MODEL_SHORT_NAME}/${TASK_12}/final_model" \
-    taskpool=clip-vit-single-task_${DATA_1}
+    taskpool=clip-vit-single-task_${TASK_1}
 
 fusion_bench \
     --config-dir ${SCRIPT_DIR}/config \
     method=dummy \
     modelpool=CLIPVisionModelPool/clip-vit-base-patch32_individual \
     +modelpool.modelpool.models._pretrained_.pretrained_model_name_or_path="${SCRIPT_DIR}/outputs/${MODEL_SHORT_NAME}/${TASK_12}/final_model" \
-    taskpool=clip-vit-single-task_${DATA_2}
+    taskpool=clip-vit-single-task_${TASK_2}
 
 fusion_bench \
     --config-dir ${SCRIPT_DIR}/config \
     method=dummy \
     modelpool=CLIPVisionModelPool/clip-vit-base-patch32_individual \
     +modelpool.modelpool.models._pretrained_.pretrained_model_name_or_path="${SCRIPT_DIR}/outputs/${MODEL_SHORT_NAME}/${TASK_21}/final_model" \
-    taskpool=clip-vit-single-task_${DATA_1}
+    taskpool=clip-vit-single-task_${TASK_1}
 
 fusion_bench \
     --config-dir ${SCRIPT_DIR}/config \
     method=dummy \
     modelpool=CLIPVisionModelPool/clip-vit-base-patch32_individual \
     +modelpool.modelpool.models._pretrained_.pretrained_model_name_or_path="${SCRIPT_DIR}/outputs/${MODEL_SHORT_NAME}/${TASK_21}/final_model" \
-    taskpool=clip-vit-single-task_${DATA_2}
+    taskpool=clip-vit-single-task_${TASK_2}
 
 
 
